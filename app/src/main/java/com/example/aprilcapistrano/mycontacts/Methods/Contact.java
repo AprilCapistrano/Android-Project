e6172dadd89049d17d3e70e29c362b5f370c5ce5
lllegada <shiruba.rein15@gmail.com>
2018-08-24 16:28:10 +0800

    added Contact class/entity

---
+++ app/src/main/java/com/example/aprilcapistrano/mycontacts/Methods/Contact.java
@@ -0,0 +1,62 @@
+package com.example.aprilcapistrano.mycontacts.Methods;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "contact_table")
+public class Contact {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    public int id;
+
+    @ColumnInfo(name = "name")
+    @NonNull
+    public String name;
+
+    @ColumnInfo(name = "number")
+    @NonNull
+    public String number;
+
+    @ColumnInfo(name = "email")
+    @NonNull
+    public String email;
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(@NonNull String number) {
+        this.number = number;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+}
