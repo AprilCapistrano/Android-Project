7a45c83e7fc4b27bb297036e9e35e01a80a626d3
April Gail Capistrano <april.capistrano@awsys-i.com>
2018-08-24 17:24:12 +0800

    Contact Room Database added PopulateDbAsync

--- app/src/main/java/com/example/aprilcapistrano/mycontacts/Methods/ContactRoomDatabase.java
+++ app/src/main/java/com/example/aprilcapistrano/mycontacts/Methods/ContactRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.aprilcapistrano.mycontacts.Methods;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Contact.class}, version = 1)
 public abstract class ContactRoomDatabase extends RoomDatabase {
@@ -18,10 +21,39 @@ public abstract class ContactRoomDatabase extends RoomDatabase {
                     //CREATE DATABASE
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             ContactRoomDatabase.class, "contacts_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final ContactDAO mDao;
+
+        PopulateDbAsync(ContactRoomDatabase db) {
+            mDao = db.contactDAO();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            //for delete
+            mDao.deleteAll();
+
+            //for Adding
+            Contact contact = new Contact("FirstName LastName", "0909 123 1234", "fname@gmail.com");
+            mDao.insert(contact);
+            return null;
+        }
     }
 }
