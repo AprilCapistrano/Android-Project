832e3332f353b29ad2ee7620955f90eae49dce30
April Gail Capistrano <april.capistrano@awsys-i.com>
2018-08-24 15:48:28 +0800

    ContactDAO

---
+++ app/src/main/java/com/example/aprilcapistrano/mycontacts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aprilcapistrano.mycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/aprilcapistrano/mycontacts/Methods/ContactDAO.java
@@ -0,0 +1,22 @@
+package com.example.aprilcapistrano.mycontacts.Methods;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactDAO {
+    @Insert
+    void insert(Contacts contacts);
+
+    @Query("DELETE FROM contacts_table")
+    void deleteAll();
+
+    @Query("SELECT * from contacts_table ORDER BY contacts ASC")
+    List<Contacts> getAllContacts();
+
+    @Query("SELECT name from contacts_table ORDER BY name ASC")
+    List<String> getAllNames();
+}
