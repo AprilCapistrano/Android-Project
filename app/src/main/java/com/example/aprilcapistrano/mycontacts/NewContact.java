2e8fc973232c0a093d3c0788400a08b0f379cd62
faye <jasielfayedumdum@gmail.com>
2018-08-27 11:58:36 +0800

    Addition of Fields on New Contact

--- app/src/main/java/com/example/aprilcapistrano/mycontacts/NewContact.java
+++ app/src/main/java/com/example/aprilcapistrano/mycontacts/NewContact.java
@@ -11,23 +11,30 @@ import android.widget.EditText;
 public class NewContact extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
 
-    private EditText mEditContactView;
+    private EditText mEditContactName, mEditContactNum, mEditContactEmail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
-        mEditContactView = findViewById(R.id.edit_word);
+        mEditContactName = findViewById(R.id.edit_word);
+        mEditContactEmail = findViewById(R.id.edit_email);
+        mEditContactNum = findViewById(R.id.edit_number);
 
         final Button button = findViewById(R.id.button_save);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(mEditContactView.getText())) {
+                if (TextUtils.isEmpty(mEditContactName.getText()) || TextUtils.isEmpty(mEditContactEmail.getText())
+                        || TextUtils.isEmpty(mEditContactNum.getText())) {
                     setResult(RESULT_CANCELED, replyIntent);
                 } else {
-                    String word = mEditContactView.getText().toString();
-                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    String name = mEditContactName.getText().toString();
+                    String email = mEditContactEmail.getText().toString();
+                    String num = mEditContactNum.getText().toString();
+                    replyIntent.putExtra(EXTRA_REPLY, name);
+                    replyIntent.putExtra(EXTRA_REPLY, email);
+                    replyIntent.putExtra(EXTRA_REPLY, num);
                     setResult(RESULT_OK, replyIntent);
                 }
                 finish();
